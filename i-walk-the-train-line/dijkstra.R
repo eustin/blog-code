5abd298ae7c71c36e8025f6fe0f4094d86764737
eustin <eustin.jvans@gmail.com>
2019-04-24 07:07:16 +1000

    remove make_weighted_adj_list as it already exists in the repo. source it instead

--- i-walk-the-train-line/dijkstra.R
+++ i-walk-the-train-line/dijkstra.R
@@ -1,12 +1,9 @@
-# this function can be found in this folder as well!
+# these function can be found in this folder as well!
 source('get_path_length.R')
+source('make_weighted_adj_list.R')
 
 # constants
 LARGE_NUMBER <- 999999
-
-# osm_graph <- igraph_highways_and_nodes
-# source_node <- nearest_to_newtown
-# destination_node <- nearest_to_small_bar
 
 dijkstra_shortest_path <- function(osm_graph, source_node, destination_node) {
   
@@ -55,48 +52,6 @@ dijkstra_shortest_path <- function(osm_graph, source_node, destination_node) {
   return(results)
 }
 
-make_weighted_adj_list <- function(igraph_obj, edge_attr='weight') {
-  
-  print('creating weighted adjacency list')
-  edge_list <- as.data.table(as_edgelist(igraph_obj))
-  names(edge_list) <- c('from', 'to')
-  edge_list[, weight:=E(igraph_obj)$weight]
-  
-  # some duplicated edges - e.g. 20827842
-  edge_list <- unique(edge_list)
-  
-  # create hashed environments for fast subsetting 
-  nodes_split_from <- split(edge_list, edge_list$from)
-  nodes_split_to <- split(edge_list, edge_list$to)
-  
-  node_from_env <- list2env(nodes_split_from, hash=TRUE)
-  node_to_env <- list2env(nodes_split_to, hash=TRUE)
-  
-  # create preallocated list for weighted adj list we will be creating
-  weighted_adj_list <- vector('list', vcount(igraph_obj))
-  names(weighted_adj_list) <- names(V(igraph_obj))
-
-  for (node in names(weighted_adj_list)) {
-    edge_subset <- node_from_env[[node]]
-    if (!is.null(edge_subset)) {
-      weights <- edge_subset$weight
-      names(weights) <- edge_subset$to
-      weighted_adj_list[[node]] <- weights
-    }
-    
-    edge_subset <- node_to_env[[node]]
-    if (!is.null(edge_subset)) {
-      already_appened <- names(weighted_adj_list[[node]])
-      not_already_appened <- !edge_subset$from %in% already_appened
-
-      if (length(not_already_appened) == 0) next
-      weights <- edge_subset$weight[not_already_appened]
-      names(weights) <- edge_subset$from[not_already_appened]
-      weighted_adj_list[[node]] <- append(weighted_adj_list[[node]], weights)
-    } 
-  }
-  return(weighted_adj_list)
-}
 
 remove_min_dist_node <- function(vec_of_nodes, min_dist_node_id) {
   min_dist_node_idx <- which(vec_of_nodes == min_dist_node_id)
@@ -104,15 +59,18 @@ remove_min_dist_node <- function(vec_of_nodes, min_dist_node_id) {
   return(vec_of_nodes)
 }
 
+
 get_current_dist_to_adj_node <- function(vec_of_distances, adjacent_node_id) {
   return(vec_of_distances[[adjacent_node_id]])  
 }
+
 
 get_adjacent_nodes_left_to_process <- function(weighted_adj_list, vec_of_nodes, min_dist_node_id) {
   adjacent_nodes <- weighted_adj_list[[min_dist_node_id]]
   adjacent_and_left_to_process <- intersect(names(adjacent_nodes), vec_of_nodes)
   return(adjacent_nodes[adjacent_and_left_to_process])
 }
+
 
 find_min_dist_node <- function(vec_of_nodes, vec_of_distances) {
   distances_left_vec <- distances_to_process(vec_of_nodes, vec_of_distances)
@@ -122,11 +80,13 @@ find_min_dist_node <- function(vec_of_nodes, vec_of_distances) {
   return(result)
 }
 
+
 distances_to_process <- function(vec_of_nodes, vec_of_distances) {
   distances_left <- intersect(names(vec_of_distances), vec_of_nodes)
   distances_left_vec <- vec_of_distances[distances_left]
   return(distances_left_vec)
 }
+
 
 get_min_dist_id <- function(vec_of_distances, min_distance) {
   min_distance_idx <- match(min_distance, vec_of_distances)
@@ -134,10 +94,12 @@ get_min_dist_id <- function(vec_of_distances, min_distance) {
   return(min_distance_node_id)
 }
 
+
 nodes_left_to_process <- function(vec_of_nodes) {
   num_nodes_not_na <- sum(!is.na(vec_of_nodes))
   return(num_nodes_not_na > 0)
 }
+
 
 create_predecessors_vec <- function(osm_graph) {
   num_nodes <- get_num_nodes(osm_graph)
@@ -146,14 +108,17 @@ create_predecessors_vec <- function(osm_graph) {
   return(predecessors_vec)
 }
 
+
 get_num_nodes <- function(osm_graph) {
   return(vcount(osm_graph))
 }
+
 
 get_node_names <- function(osm_graph) {
   vertices <- V(osm_graph)
   return(names(vertices))
 }
+
 
 create_distances_vec <- function(osm_graph, source_node_id) {
   num_nodes <- vcount(osm_graph)
@@ -163,10 +128,12 @@ create_distances_vec <- function(osm_graph, source_node_id) {
   return(distances_vec)
 }
 
+
 initialise_distances_vector <- function(distances_vector, source_node_id) {
   distances_vector[[source_node_id]] <- 0
   return(distances_vector)
 }
+
 
 recover_shortest_path <- function(source_node_id, destination_node_id, vec_of_predecessors, vec_of_distances) {
   path <- character()
